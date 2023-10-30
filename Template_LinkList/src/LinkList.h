7cd53d2674824d2b97f64592300a026850f1c707
niina <C27shu@gmail.com>
2023-10-30 12:31:48 +0800

    update!

--- Template_LinkList/src/LinkList.h
+++ Template_LinkList/src/LinkList.h
@@ -72,26 +72,33 @@ public:
     Node<T>* curNode() const { return _cur_node; }
     LinkList<T>* next() const { return _cur_node->next(); }
     LinkList<T>* prev() const { return _cur_node->prev(); }
-    Node<T>* getIndex(const T& data) const {
+    Node<T>* Go(size_t index) const {
+        Node<T>* cur_node = _head;
+        for (size_t i = 0; i < index; i++) {
+            cur_node = cur_node->next();
+        }
+        return cur_node;
+    }
+    Node<T>* locate(const T& data) {
         Node<T>* cur_node = _head;
         while (cur_node != nullptr && cur_node->data() != data) {
             cur_node = cur_node->next();
         }
         return cur_node;
     }
-    T& locate(const T& data) {
-        Node<T>* cur_node = _head;
-        while (cur_node != nullptr && cur_node->data() != data) {
-            cur_node = cur_node->next();
-        }
-        return cur_node->data();
-    }
-    T& index(size_t index) const{
+    T& index(size_t index) const {
         Node<T>* cur_node = _head;
         for (size_t i = 0; i < index; i++) {
             cur_node = cur_node->next();
         }
         return cur_node->data();
+    }
+    Node<T>* getNode(const T& data) const {
+        Node<T>* cur_node = _head;
+        while (cur_node != nullptr && cur_node->data() != data) {
+            cur_node = cur_node->next();
+        }
+        return cur_node;
     }
     
     // Add data for the LinkList class
